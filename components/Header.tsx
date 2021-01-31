cfff1e63b8371f1c19d8348d7d61466fc78bbcb1
Rishabh Jain <rishabh@codeation.io>
2021-01-31 15:41:25 +0530

    chore: header component

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Header: FC = () => {
+  return (
+    <>
+      <header className="h-10 bg-blue-200">Header</header>
+    </>
+  );
+};
+
+export default Header;

f38de9f2a3199be71ddcee08f84a4e3514931422
Rishabh Jain <rishabh@codeation.io>
2021-02-01 11:07:14 +0530

    feat: using meta component in Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,12 @@
-import Header from 'components/Header';
+// import Header from 'components/Header';
+import Meta from 'components/Meta';
 import Nav from 'components/Nav';
 import { FC } from 'react';
 
 const Layout: FC = ({ children }) => {
   return (
     <>
+      <Meta />
       <main className="flex flex-row">
         <Nav />
         <section className="flex flex-grow flex-col">

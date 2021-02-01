10a83efcb8a5b904dd1099f4a56afadcb409fb1a
Rishabh Jain <rishabh@codeation.io>
2021-02-01 10:58:10 +0530

    feat: NavItems

--- components/Nav/NavItem.tsx
+++ components/Nav/NavItem.tsx
@@ -2,9 +2,7 @@ import { FC } from 'react';
 
 const NavItem: FC = ({ children }) => {
   return (
-    <li className="p-2 mb-2 text-gray-300 text-center border-b-2 border-gray-800 border shadow rounded-lg">
-      {children}
-    </li>
+    <li className="p-2 mb-2 text-center border-4 border-b-8 shadow-2xl rounded-lg">{children}</li>
   );
 };
 

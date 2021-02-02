b9123544dbe246fa20be553f5970a597163e4db4
Rishabh Jain <rishabh@codeation.io>
2021-02-02 16:35:45 +0530

    feat: using btn classes

--- components/Nav/NavItem.tsx
+++ components/Nav/NavItem.tsx
@@ -1,9 +1,7 @@
 import { FC } from 'react';
 
 const NavItem: FC = ({ children }) => {
-  return (
-    <li className="p-2 mb-2 text-center border-4 border-b-8 shadow-2xl rounded-lg">{children}</li>
-  );
+  return <li className="btn mb-2 border-4 border-b-8">{children}</li>;
 };
 
 export default NavItem;

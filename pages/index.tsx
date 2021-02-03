3548a87e35904e80b03871730a64e22492a5a9c7
Rishabh Jain <rishabh@codeation.io>
2021-02-03 14:20:48 +0530

    feat: create task page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
+import CreateTask from 'components/form/tasks/CreateTask';
 import Layout from 'components/Layout';
 import { FC } from 'react';
 
 const Home: FC = () => {
   return (
     <div className="h-screen">
-      <Layout>Layout</Layout>
+      <Layout>
+        <section className="flex-center h-screen">
+          <CreateTask />
+        </section>
+      </Layout>
     </div>
   );
 };

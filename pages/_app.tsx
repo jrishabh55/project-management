a06f3104dd269a0ad7c06cf51d32a5db628630bb
Rishabh Jain <rishabh@codeation.io>
2021-02-07 12:54:08 +0530

    feat: implemented the useReducer and context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import '../styles/globals.scss';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { stateReducer } from 'context/reducers/stateReducer';
+import StateContext, { defaultState } from 'context/stateContext';
+import { ReactElement, useReducer } from 'react';
+
+function MyApp({ Component, pageProps }): ReactElement {
+  const [state, dispatch] = useReducer(stateReducer, defaultState);
+  return (
+    <StateContext value={[state, dispatch]}>
+      <Component {...pageProps} />
+    </StateContext>
+  );
 }
 
 export default MyApp;

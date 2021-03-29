a71a5461c569dd6ab1d9e3ee2e7070314de2c58e
Rishabh Jain <rishabh@codeation.io>
2021-03-29 23:32:59 +0530

    chore: firebase initilization loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
+import { Card } from 'components/Card';
 import CreateTask from 'components/form/tasks/CreateTask';
 import Layout from 'components/Layout';
-import { FC } from 'react';
+import { useFirebase } from 'context/firebase';
+import { useRouter } from 'next/router';
+import { FC, useEffect } from 'react';
 
 const Home: FC = () => {
+  const { user, isInitialized: initialized } = useFirebase();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      router.replace('/login');
+    }
+  }, [user]);
+
+  if (!initialized) {
+    return <Card className="h-screen flex-center">Loading</Card>;
+  }
+
   return (
     <div className="h-screen">
       <Layout>

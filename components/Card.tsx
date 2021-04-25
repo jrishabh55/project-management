79eb8e68edb78b2f15200b629d71bd10d210ab44
Rishabh Jain <rishabh@codeation.io>
2021-04-25 12:59:49 +0530

    feat: card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,13 +1,16 @@
 import cx from 'classnames';
 import { DetailedHTMLProps, FC, HTMLAttributes } from 'react';
 
-export const Card: FC<DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>> = ({
-  children,
-  className,
-  ...props
-}) => {
+export const Card: FC<
+  DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
+    sm?: boolean;
+    rounded?: boolean;
+  }
+> = ({ children, className, rounded = false, sm = false, ...props }) => {
   return (
-    <div className={cx('p-10 nm-convex-gray-600', className)} {...props}>
+    <div
+      className={cx('nm-convex-gray-600 m-4', sm ? 'p-2' : 'p-10', { rounded }, className)}
+      {...props}>
       {children}
     </div>
   );
